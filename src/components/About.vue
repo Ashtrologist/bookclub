f5ffb5c2f77aa097308efed133d9d8372489f786
Unknown <ashtonrodriquez@hotmail.com>
2018-08-01 20:50:14 -0400

    Added background image and books

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <h1>Meet the team</h1>
+  <div class="banner">
+    <h1 class="header">Meet the team</h1>
+  </div>
   <br>
 
   <div>
     <div>
       <h2> President </h2>
-      <img src="@/assets/book.jpg" alt="Sondos Hassan" height="200px" width="200px"/>
+      <img src="@/assets/sondos.jpeg" alt="Sondos Hassan" height="300px" width="300px"/>
       <h3> Sondos Hassan </h3>
     </div>
     <v-layout justify-center row>
@@ -42,5 +44,16 @@ export default {
   padding: 10px;
   margin: 15px
 }
-
+.banner{
+  min-height: 350px;
+  max-height: 350px;
+  background-image: url("https://images.unsplash.com/photo-1529070538774-1843cb3265df?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=615eb020ee1d598fdd9b5c40ede9eccf&auto=format&fit=crop&w=1650&q=80");
+}
+.header{
+  text-transform: uppercase;
+  font-size: 60px;
+  align-content: center;
+  color: white;
+  padding-top: 50px;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
-  <div justify-center class="circular">
-    <h1> Welcome to the Dead Poets Society </h1>
+  <div justify-center class="banner">
+    <h1 class="header"> Welcome to the Dead Poets Society </h1>
   </div>
   <div>
     <h2> About Us </h2>
     <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      The Dead Poets Society Book Club get’s its name from the timeless movie starring Robin Williams, where he plays the role of an influential teacher inspiring his students to surpass the seemingly mundane task of “reading” literature, rather feeling it, allowing its words to sink into the malleable minds of his students. This Book club serves the same purpose. To allow the flow of ideas, thoughts, and discussions from timeless works to dance between the minds of our members. Meetings will be intended for great minds to come together and lay out their thoughts, educated opinions, and most of all openness to more than one interpretation to varying ideas. every book is a world and every person is a world that has the potential to enrich your own world.” ~Shaykh Amr
     </p>
   </div>
 
@@ -18,55 +18,82 @@
         <h3> Meet new and interesting people </h3>
       </div>
 
+    <div class="spacing">
+      <img src="@/assets/favoritebook.jpg" alt="Book" height="300" width="400"/>
+      <h3> Share your favorite books </h3>
+      </div>
+    </div>
       <div class="spacing">
-        <img src="@/assets/calendar.jpg" height="300" width="400"/>
+        <img src="@/assets/bookclub.jpg" height="300" width="400"/>
         <h3> Weekly gatherings </h3>
       </div>
-
-      <div class="spacing">
-        <img src="@/assets/favoritebook.jpg" alt="Book" height="300" width="400"/>
-        <h3> Share your favorite books </h3>
-      </div>
-    </div>
   </v-layout>
 
-  <h1> Most popular books </h1>
+  <h1> Our favorite books </h1>
+  <div class="flexbox">
   <v-layout justify-center row fill-height>
     <div class="books">
-      <img src="@/assets/book.jpg" class="image"/>
-      <h2> Little Fires Everywhere</h2>
-      <h4> By: Random Author </h4>
-      <v-btn> Buy it on amazon </v-btn>
+      <img src="@/assets/inheritance.jpg" class="image"/>
+      <h2> Inheritance</h2>
+      <h4> By: Sharon Mualem </h4>
+      <v-btn
+        dark
+        href="https://www.amazon.com/Inheritance-Genes-Change-Lives-Lives/dp/1455549436/ref=sr_1_1?s=books&ie=UTF8&qid=1533169607&sr=1-1&keywords=inheritance+sharon+moalem"
+      >
+       Buy it on amazon
+      </v-btn>
     </div>
 
      <div class="books">
-      <img src="@/assets/book.jpg" class="image"/>
-      <h2> Educated a memoir</h2>
-      <h4> By: Random Author </h4>
-      <v-btn> Buy it on amazon </v-btn>
+      <img src="@/assets/1984.jpg" class="image"/>
+      <h2> 1984 </h2>
+      <h4> By: George Orwell</h4>
+      <v-btn
+        dark
+        href="https://www.amazon.com/1984-Signet-Classics-George-Orwell/dp/0451524934/ref=sr_1_1?s=books&ie=UTF8&qid=1533170874&sr=1-1&keywords=1984+by+george+orwell&smid=ATVPDKIKX0DER"
+      >
+       Buy it on amazon
+       </v-btn>
     </div>
 
     <div class="books">
-      <img src="@/assets/book.jpg" class="image" alt="Book"/>
-      <h2> The hate you give</h2>
-      <h4> By: Random Author </h4>
-      <v-btn> Buy it on amazon </v-btn>
+      <img src="@/assets/essays.jpg" class="image" alt="Book"/>
+      <h2> Essays</h2>
+      <h4> By: Michel de Montaigne </h4>
+      <v-btn
+        dark
+        href="https://www.amazon.com/Montaigne-Essays-Michel/dp/014017897X/ref=sr_1_2?s=books&ie=UTF8&qid=1533170750&sr=1-2&keywords=essays+michel+de+montaigne"
+      >
+       Buy it on amazon
+     </v-btn>
     </div>
 
     <div class="books">
-      <img src="@/assets/book.jpg" class="image"/>
-      <h2> The hate you give</h2>
-      <h4> By: Random Author </h4>
-      <v-btn> Buy it on amazon </v-btn>
+      <img src="@/assets/wind.jpg" class="image"/>
+      <h2> The Name of the Wind</h2>
+      <h4> By: Patrick Rothfuss </h4>
+      <v-btn
+        dark
+        href="https://www.amazon.com/Name-Wind-Patrick-Rothfuss/dp/0756404746/ref=sr_1_1?s=books&ie=UTF8&qid=1533168766&sr=1-1&keywords=the+name+of+the+wind"
+      >
+      Buy it on amazon
+      </v-btn>
+
     </div>
 
      <div class="books">
-      <img src="@/assets/book.jpg" class="image"/>
-      <h2> The hate you give</h2>
-      <h4> By: Random Author </h4>
-      <v-btn> Buy it on amazon </v-btn>
+      <img src="@/assets/kybalion.jpeg" class="image"/>
+      <h2> The Kybalion</h2>
+      <h4> By: Three Initiates </h4>
+      <v-btn
+        dark
+        href="https://www.amazon.com/Kybalion-Centenary-Three-Initiates/dp/0143131680/ref=sr_1_2?ie=UTF8&qid=1533168071&sr=8-2&keywords=the+kybalion"
+      >
+        Buy it on amazon
+      </v-btn>
     </div>
   </v-layout>
+  </div>
 </div>
 
 </template>
@@ -85,16 +112,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.circular{
 
-  min-height: 250px;
+.banner{
+  min-height: 350px;
+  max-height: 350px;
   background-image: url("https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=954560871844da528c60343dc344ccbf&auto=format&fit=crop&w=1950&q=80");
-  
+}
+.header{
+  text-transform: uppercase;
+  font-size: 60px;
+  align-content: center;
+  color: white;
+  padding-top: 50px;
 }
 .flexbox{
   display: flex;
-  overflow: scroll;
-  /* align-content: center; */
+  overflow: auto;
   margin: 10px;
 }
 .spacing{
